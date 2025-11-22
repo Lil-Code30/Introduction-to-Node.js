473b892bf82b5d745cadd1acc74fd04990e262db
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-11-22 09:28:32 -0500

    Add notes.txt and enhance server response with file info
    Created a new notes.txt file and updated server.js to respond with file path information (dirname, basename, extension, and fileName) for notes.txt using the path module.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import http from "node:http";
+import path, { basename } from "node:path";
 
 const hostname = "127.0.0.1";
 const port = 8001;
+const notes = "./notes.txt";
 
 const server = http.createServer((req, res) => {
   const { headers, method, url } = req;
@@ -17,8 +19,14 @@ const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/plain");
 
   const resBody = { headers, method, url };
+  const notesData = {
+    dirname: path.dirname(notes),
+    basename: path.basename(notes),
+    extension: path.extname(notes),
+    fileName: path.basename(notes, path.extname(notes)),
+  };
 
-  res.write(JSON.stringify(resBody));
+  res.write(JSON.stringify(notesData));
   res.end();
 });
 
